70e29dbada2238c986a1bf379413d8954dcd04ac
ibnjunaid <osamabinjunaid36@gmail.com>
2021-06-18 01:00:49 +0530

    Parser working need to test and add more functionality

--- examples/test.ts
+++ examples/test.ts
@@ -1,33 +1,12 @@
 import * as fs from "fs/promises";
-import { FeatureCollection, geoJSON } from '../index';
-
-// async function init(){
-//     const data = await fs.readFile("/home/osama/Desktop/Geojson/data/sample1.geojson","utf-8");
-//     // console.log (data)
-//     let data1 = {name : "Osama"}
-//     let x = JSON.parse(data); 
-//     let l = new FeatureCollection(x);
-//     console.log(l.findFeatureByProperty({
-//         name : "Osama"
-//     }));
-// }
-
-// async function Block_AA_I(){
-//     const data = await fs.readFile("/home/osama/Desktop/Geojson/Closed Area Feature Layers/Block_AA_I.geojson","utf-8");
-//     let x = JSON.parse(data); 
-//     let l = new FeatureCollection(x);
-//     console.log(l.findFeatureByProperty({
-//         Id : 1,
-//         Block_ID: '30'
-//     }));
-// }
+import { FeatureCollection, createUsingFile, GeoJSON } from '../index';
 
 async function xyz () {
-    const x = await geoJSON.createByFile("/home/osama/Desktop/Geojson/Closed Area Feature Layers/Block_AA_I.geojson");
-    let t = x.findFeatureByProperty({'Id':1})
-    console.log(t.length);
+    const x = await GeoJSON.createUsingFile("/home/osama/Desktop/Geojson/data/sample1.geojson")
+    const y = await GeoJSON.createUsingFile("/home/osama/Desktop/Geojson/data/sample1.geojson")
+    y.Remove({geometry:{type:"Point"}})
+    await y.Save("test.geoJSON")
+    console.log(x.data.features.length,y.data.features.length)
 }
 
 xyz().catch(console.error);
-
-// Block_AA_I().then(console.log).catch(console.error);
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import * as fs from "fs/promises";
 
 enum geometryTypes {
-    Point = "POINT", 
-    MultiPoint = "MULTIPOINT", 
+    Point = "POINT",
+    MultiPoint = "MULTIPOINT",
     LineString = "LINESTRING",
-    MultiLineString = "MULTILINESTRING", 
-    Polygon = "POLYGON", 
+    MultiLineString = "MULTILINESTRING",
+    Polygon = "POLYGON",
     MultiPolygon = "MULTIPOLYGON",
     GeometryCollection = "GEOMETRYCOLLECTION"
 }
@@ -18,30 +18,30 @@ enum collectionTypes {
 //   For type "Point", the "coordinates" member is a single position.
 
 interface pointCoordinates {
-    lat : number,
-    lang : number,
-    alt ?: number
+    lat: number,
+    lang: number,
+    alt?: number
 }
 
 //  For type "MultiPoint", the "coordinates" member is an array of
 //  positions.
 
 interface multiPointCoordinates {
-    [index : number] : pointCoordinates
+    [index: number]: pointCoordinates
 }
 
 //   For type "LineString", the "coordinates" member is an array of two or
 //   more positions.
 
-interface lineStringCoordinates{
-    [index : number] : pointCoordinates //FIXME:  add strictness for array of two or more positions.
+interface lineStringCoordinates {
+    [index: number]: pointCoordinates //FIXME:  add strictness for array of two or more positions.
 }
 
 //   For type "MultiLineString", the "coordinates" member is an array of
 //   LineString coordinate arrays
 
-interface multiLineStringCoordinates{
-    [index : number] : lineStringCoordinates
+interface multiLineStringCoordinates {
+    [index: number]: lineStringCoordinates
 }
 
 /*
@@ -58,8 +58,8 @@ interface multiLineStringCoordinates{
         holes are clockwise.
 */
 //TODO: make linar ring coordinates to satisfy above conditions
-interface linearRingCoordinates{
-    [index : number] : lineStringCoordinates
+interface linearRingCoordinates {
+    [index: number]: lineStringCoordinates
 }
 
 /*
@@ -71,8 +71,8 @@ interface linearRingCoordinates{
       exterior ring bounds the surface, and the interior rings (if
       present) bound holes within the surface.
 */
-interface polygonCoordinates{
-    [index : number] :  linearRingCoordinates
+interface polygonCoordinates {
+    [index: number]: linearRingCoordinates
 }
 
 /*
@@ -82,53 +82,222 @@ interface polygonCoordinates{
 
 */
 
-interface MultiPolygonCoordinates{
-    [index : number] : polygonCoordinates
+interface MultiPolygonCoordinates {
+    [index: number]: polygonCoordinates
 }
 
 
 
-interface feature{
-    type : collectionTypes.Feature
-    geometry : {
-        type : geometryTypes,
-        coordinates : pointCoordinates | multiPointCoordinates |
+interface feature {
+    type: collectionTypes.Feature
+    geometry: {
+        type: geometryTypes,
+        coordinates: pointCoordinates | multiPointCoordinates |
         lineStringCoordinates | multiLineStringCoordinates |
         polygonCoordinates | MultiPolygonCoordinates
     }
-    properties : {
-        [index:string] : any
+    properties: {
+        [index: string]: any
     }
 }
 
 
-export interface featureCollection{
-    type : collectionTypes.FeatureCollection,    
-    features : Array<feature>; // Make features an Array of all possible types
+export interface featureCollection {
+    type: collectionTypes.FeatureCollection,
+    features: feature[];
 }
 
+interface Query {
+    geometry?: {
+        type: string
+    },
+    properties?: {
+        [index: string]: any
+    }
+}
 export class FeatureCollection {
-    data : featureCollection;
-    constructor(x: featureCollection){
+    data: featureCollection;
+    filePath : string
+
+    constructor(x: featureCollection, filePath :string) {
         this.data = x;
+        this.filePath = filePath
     }
-    findFeatureByProperty = ( x : {[index:string]:any}) => {
+
+    public Find = (feature?: Query) => {
+        if (feature === undefined) {
+            return this.data.features
+        } else if (feature.geometry?.type === null && feature.properties === null) {
+            return this.data.features
+        } else if (feature.geometry?.type === null) {
+            return this.FindByProperty(feature?.properties || {});
+        } else if (feature.properties === null) {
+            return this.FindByGeometry(feature?.geometry || {})
+        } else {
+            return this.data.features.filter(f => {
+                if (f.geometry.type !== feature.geometry?.type) {
+                    return false;
+                } else {
+                    for (let [key, value] of Object.entries(feature?.properties || {})) {
+                        if (f.properties[key] != value) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            })
+        }
+    }
+
+
+    public FindByProperty = (x: { [index: string]: any }) => {
         return this.data.features.filter(f => {
-            for (let [key,value] of Object.entries(x)){
-                if(f.properties[key] != value){
+            for (let [key, value] of Object.entries(x)) {
+                if (f.properties[key] != value) {
                     return false;
                 }
             }
             return true;
         })
     }
-    // Find and Add properties
-    // 
+
+    public FindByGeometry = (d: { type?: string }) => {
+        return this.data.features.filter(f => f.geometry.type === d?.type)
+    }
+
+    private remove = (feature: Query) => {
+        if (feature.geometry === undefined) {
+            return this.findAndremoveByProperty(feature?.properties || {})
+        } else if (feature.properties === undefined) {
+            return this.findAndremoveByGeometry(feature.geometry);
+        } else {
+            return this.data.features.filter(f => {
+                if (f.geometry.type !== feature.geometry?.type) {
+                    return true;
+                } else {
+                    for (let [key, value] of Object.entries(feature?.properties || {})) {
+                        if (f.properties[key] != value) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            })
+        }
+    }
+
+    private findAndremoveByProperty = (properties: { [index: string]: any }) => {
+        return this.data.features.filter(f => {
+            for (let [key, value] of Object.entries(properties)) {
+                if (f.properties[key] != value) {
+                    return true;
+                }
+            }
+            return false;
+        })
+    }
+
+    private findAndremoveByGeometry = (geometry: { type: string; }) => {
+        return this.data.features.filter(f => f.geometry.type !== geometry?.type)
+    }
+
+    private update = (feature: Query, update: Query) => {
+        if (feature === undefined) {
+            return this.findAndUpdateAll(update || {});
+        } else if (feature.geometry === undefined) {
+            return this.findAndUpdateByProperties(feature.properties || {}, update || {})
+        } else if (feature.properties === undefined) {
+            return this.findAndUpdateByGeometry(feature.geometry, update || {})
+        }
+        return this.findAndUpdateAll(update || {});
+    }
+
+    private findAndUpdateAll(update: Query) {
+        return this.data.features.map(f => {
+            for (const [key, value] of Object.entries(update?.properties || {})) {
+                f.properties[key] = value;
+            }
+            if (update?.geometry?.type) {
+                f.geometry.type = update.geometry.type as geometryTypes;
+            }
+            return f!;
+        })
+    }
+
+    private findAndUpdateByGeometry(geometry: { type: string; }, update: Query) {
+        return this.data.features.map(f => {
+            if (f.geometry.type === geometry.type) {
+                for (const [key, value] of Object.entries(update?.properties || {})) {
+                    f.properties[key] = value;
+                }
+                if (update?.geometry?.type) {
+                    f.geometry.type = update.geometry.type as geometryTypes
+                }
+            }
+            return f!;
+        })
+    }
+
+    private findAndUpdateByProperties(properties: { [index: string]: any; }, update: Query) {
+        return this.data.features.map(f => {
+            for (const [key, value] of Object.entries(properties)) {
+                if (f.properties[key] !== value) {
+                    return f;
+                }
+                for (const [key, value] of Object.entries(update?.properties || {})) {
+                    f.properties[key] = value;
+                }
+                if (update?.geometry?.type) {
+                    f.geometry.type = update.geometry.type as geometryTypes;
+                }
+            }
+            return f
+        })
+    }
+
+    public GetAllFeatures = () => {
+        return this.data.features
+    }
+    public Update(feature: Query, update: Query){
+        this.data.features = this.update(feature, update);
+    }
+    public Remove(feature:Query){
+        this.data.features = this.remove(feature)
+    }
+    public async Save(filePath ?: string){
+        const realpath = filePath === undefined ? this.filePath : filePath;
+        try{
+            await fs.writeFile(realpath,JSON.stringify(this.data))
+        } catch (error){
+            throw error;
+        }
+    }
+
 }
-export class geoJSON {
-    static createByFile  = async (filePath : string) =>{
-        const data = await fs.readFile(filePath,'utf-8');
+
+//-----------------------------------------------------Exports below this line-----------------------------
+
+
+export const createUsingFile = async (filePath: string) => {
+    const data = await fs.readFile(filePath, 'utf-8');
+    const json = JSON.parse(data);
+    return new FeatureCollection(json,filePath)
+}
+
+export const createFromObject = (data: featureCollection , filePath : string) => {
+    return new FeatureCollection(data, filePath);
+}
+
+
+export class GeoJSON {
+    static createUsingFile = async (filePath: string) => {
+        const data = await fs.readFile(filePath, 'utf-8');
         const json = JSON.parse(data);
-        return new FeatureCollection(json)
+        return new FeatureCollection(json,filePath)
+    }
+    static createFromObject = (data: featureCollection, filePath : string) => {
+        return new FeatureCollection(data,filePath);
     }
 }
+
+
